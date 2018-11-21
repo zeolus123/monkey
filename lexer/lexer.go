705a6d400d83890b1c5e7e94f4491fe8fc20ade2
Zachery Spencer <spencer.z@hotmail.com>
2018-11-20 21:35:09 -0400

    taking into account any whitespace

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -14,6 +14,8 @@ type Lexer struct {
 func (l *Lexer) NextToken() token.Token {
 	var tok token.Token
 
+	l.skipWhitespace()
+
 	switch l.ch {
 	case '=':
 		tok = newToken(token.ASSIGN, l.ch)
@@ -45,6 +47,12 @@ func (l *Lexer) NextToken() token.Token {
 	}
 	l.readChar()
 	return tok
+}
+
+func (l *Lexer) skipWhitespace() {
+	for l.ch == ' ' || l.ch =='\t' || l.ch == '\n' || l.ch =='\r' {
+		l.readChar()
+	}
 }
 
 func (l *Lexer) readIdentifier() string {
